aea6ba693479a21d406b331193de8c27fb0325cc
Nuno Job <nunojobpinto@gmail.com>
2012-03-27 15:12:07 +0100

    [test] adds tests that should work but do not

--- test/server/http/attach-test.js
+++ test/server/http/attach-test.js
@@ -32,15 +32,17 @@ function assertMethod (uri, method) {
   method = method || "GET";
   return {
     topic: function () {
-      request({ uri: 'http://localhost:9091/' + uri, method: method },
-        this.callback);
+      request({ uri: 'http://localhost:9091/' + uri, 
+                method: method, json: true }, this.callback);
     },
-    "should respond with `this.data`": function (err, res, body) {
+    "should respond with `this.data` if not head": function (err, res, body) {
       assert.isNull(err);
       assert.equal(res.statusCode, 200);
       // Why the body needs to be stringified, I don't know.
       // Someone should look into this.
-      assert.deepEqual(JSON.parse(body), [1, 2, 3]);
+      if (method !== 'HEAD') {
+        assert.deepEqual(body, [1, 2, 3]);
+      }
     }
   };
 }
@@ -50,7 +52,11 @@ vows.describe('director/server/http/attach').addBatch({
     "instantiated with a Routing table": {
       topic: new director.http.Router({
         '/hello': {
-          get: helloWorld
+          get: helloWorld,
+          patch: helloWorld
+        },
+        '/custom': {
+          on: helloWorld
         }
       }),
       "should have the correct routes defined": function (router) {
@@ -66,7 +72,11 @@ vows.describe('director/server/http/attach').addBatch({
           var server = createServer(router);
           server.listen(9091, this.callback);
         },
-        "a request to hello": assertMethod('hello')
+        "a request to hello": assertMethod('hello'),
+        "a head request to hello": assertMethod('hello', 'HEAD'),
+        "a patch request to hello": assertMethod('hello', 'PATCH'),
+        "a GET request to custom": assertMethod('custom', 'GET'),
+        "a custom request to custom": assertMethod('custom', 'XYZ')
       }
     }
   }
